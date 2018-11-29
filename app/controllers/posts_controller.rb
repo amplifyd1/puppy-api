c152a521a370a1941ed7359bfcd0ba36d4b43ac4
wesley sprague <wesleysprague@yahoo.com>
2018-11-29 12:13:13 -0500

    created post controller methods and json_response module

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,37 @@
 class PostsController < ApplicationController
+  before_action :set_post, only: [:edit, :update, :destroy]
+
+  def index
+    @posts = Posts.all
+    json_response(@posts)
+  end
+
+  def create
+    @post = Post.create!(post_params)
+    json_response(@post, :created)
+  end
+
+  def show
+    json_response(@post)
+  end
+
+  def update
+   @post.update(post_params)
+   head :no_content
+  end
+
+  def destroy
+    @post.destroy
+    head :no_content
+  end
+
+  private
+
+  def post_params
+    params.permit(:text,:photo,:created_by)
+  end
+
+  def set_post
+    @post = Post.find(params[:id])
+  end
 end
